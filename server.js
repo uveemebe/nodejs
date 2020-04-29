155933043a7ae556546edb9e129bf04697af7fc8
VICTOR MELO BARBERO <0015080@viewnext.com>
2020-04-29 16:39:43 +0200

    juego

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 // Módulos de nodejs
 const express = require('express');
 const app = express();
-const url = require('url');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 // Inicializamos los módulos
 app.set('port', process.env.PORT || 5000);
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
 let sesiones = [];
