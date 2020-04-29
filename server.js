dd3bdb805a890dd2e5a4d7a69f37d1dd106a0960
VICTOR MELO BARBERO <0015080@viewnext.com>
2020-04-29 17:09:08 +0200

    juego

--- server.js
+++ server.js
@@ -8,35 +8,27 @@ const io = require('socket.io')(server);
 app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 
-let sesiones = [];
 let puntos = [];
-let bola = {left: 128, top: 0};
+let bola = {jugador: "white", partida: "1", left: 128, top: 0};
 
 io.on('connection', function(socket) {
-    // Nos guardamos la sesión poniéndole un Id en función de la fecha y su response
-    const sesion = {id: Date.now(), socket: socket};
-    sesiones.push(sesion);
-
+    //Cuando alguien se conecta, le enviamos la situación de todos los puntos
     socket.emit('puntos', puntos);
+    //Cuando alguien se mueve, se lo enviamos a todos los demás
     socket.on('punto', function(punto) {
         puntos = puntos.filter(p => (p.jugador !== punto.jugador || p.partida !== punto.partida));
         puntos.push(punto);
         socket.broadcast.emit('puntos', [punto]);
     });
-    socket.on('disconnect', function(data) {
-        console.log(`${sesion.id} Connection closed`);
-        sesiones = sesiones.filter(s => s.id !== sesion.id);
-    });
-  });
+});
 
-// Función que se ejecuta de forma indefinida cada 1000/60 milisegundos
+// Bucle infinito que se ejecuta de forma indefinida cada 1000/60 milisegundos
 const indefinido = setInterval(() => {
     bola.top = bola.top + 4;
     if (bola.top > 256) {
         bola.top = 0;
     }
-    let punto = {jugador: "white", partida: "1", x: bola.left, y: bola.top};
-    io.sockets.emit('puntos', [punto]);
+    io.sockets.emit('puntos', [bola]);
 }, 1000/60);
 
 
