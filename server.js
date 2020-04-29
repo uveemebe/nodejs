54bed3c9284b26eb1fbaef70c9c8e11473b6ca57
VICTOR MELO BARBERO <0015080@viewnext.com>
2020-04-29 16:11:06 +0200

    juego

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Módulos de nodejs
 const express = require('express');
+const app = express();
 const url = require('url');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 // Inicializamos los módulos
-const app = express();
 app.set('port', process.env.PORT || 5000);
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,45 +18,22 @@ let sesiones = [];
 let puntos = [];
 let bola = {left: 128, top: 0};
 
-// Definimos las URL relativas que atiende nuestra aplicación y la función que procesa cada petición
-app.get('/eventos', eventos);
-app.post('/mover', mover);
-
-// Función a la que se llama cuando nos suscribimos a los eventos (por aquí se pasa una vez por sesión)
-function eventos(request, response, next) {
-    const headers = {'Content-Type': 'text/event-stream', 'Connection': 'keep-alive', 'Cache-Control': 'no-cache'};
-    response.writeHead(200, headers);
-
+io.on('connection', function(socket) {
     // Nos guardamos la sesión poniéndole un Id en función de la fecha y su response
-    const sesion = {id: Date.now(), response: response};
+    const sesion = {id: Date.now(), socket: socket};
     sesiones.push(sesion);
 
-    response.write(`data: ${JSON.stringify(puntos)}\n\n`)
-
-    // Eliminamos la sesión cuando el cliente se desconecta (cierra el navegador por ejemplo)
-    request.on('close', () => {
+    socket.emit('puntos', puntos);
+    socket.on('punto', function(punto) {
+        puntos = puntos.filter(p => (p.jugador !== punto.jugador || p.partida !== punto.partida));
+        puntos.push(punto);
+        io.sockets.emit('puntos', [punto]);
+    });
+    socket.on('disconnect', function(data) {
         console.log(`${sesion.id} Connection closed`);
         sesiones = sesiones.filter(s => s.id !== sesion.id);
     });
-}
-
-// Función a la que se llama cada vez que movemos nuestro punto
-async function mover(request, response, next) {
-    // Obtenemos los parámetros que se han pasado así "?jugador=red&partida=1&x=15&y=10" por ejemplo
-    let punto = {jugador: request.body.jugador, partida: request.body.partida, x: request.body.x, y: request.body.y};
-    response.json(punto);
-    puntos = puntos.filter(p => (p.jugador !== punto.jugador || p.partida !== punto.partida));
-    puntos.push(punto);
-    console.log(puntos);
-    return notificar(punto);
-}
-
-// Función a la que llamamos cada vez que queremos notificar algo a cada sesión
-function notificar(punto) {
-    sesiones.forEach(s => {
-        s.response.write(`data: ${JSON.stringify([punto])}\n\n`)
-    });
-}
+  });
 
 // Función que se ejecuta de forma indefinida cada 1000/60 milisegundos
 const indefinido = setInterval(() => {
@@ -62,9 +41,9 @@ const indefinido = setInterval(() => {
     if (bola.top > 256) {
         bola.top = 0;
         let punto = {jugador: "white", partida: "1", x: bola.left, y: bola.top};
-        notificar(punto);
+        io.sockets.emit('puntos', [punto]);
     }
     }, 1000/60);
 
 
-app.listen(app.get('port'), () => console.log(`Swamp Events service listening on port 3000`));
+server.listen(app.get('port'), () => console.log(`Escuchando de ${app.get('port')}`));
